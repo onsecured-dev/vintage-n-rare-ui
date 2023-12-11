4c7f9a13f3e9fb064a5c16784160639b59f57bb1
Semi Invader <=>
2023-12-11 10:24:25 -0800

    added next-themes and changed config for cleaner debugging

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  reactStrictMode: true,
+  webpack: (config, context) => {
+    config.resolve.fallback = { fs:false, net:false, tls:false }
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding|supports-color)$/,
+        }),
+      )
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
