b2c6a694e2c9c36f857b567dd932b7f4f0f70af1
Semi Invader <=>
2023-12-13 12:23:26 -0600

    support-react-hook form input

--- src/components/create/Inputs.tsx
+++ src/components/create/Inputs.tsx
@@ -6,8 +6,12 @@ export default function Input(props: {
   title: string;
   type: HTMLInputTypeAttribute;
   placeholder: string;
+  ref: React.Ref<HTMLInputElement>;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onBlur: React.FocusEventHandler<HTMLInputElement>;
+  name: string;
 }) {
-  const { title, type, placeholder } = props;
+  const { title, type, placeholder, ref, onChange, onBlur, name } = props;
   return (
     <div className="form-control">
       <label className="label">
@@ -15,8 +19,13 @@ export default function Input(props: {
       </label>
       <input
         type={type}
-        className="input input-bordered rounded-full"
+        className="input input-bordered rounded-full bg-transparent"
         placeholder={placeholder}
+        onFocus={(e) => e.currentTarget.select()}
+        ref={ref}
+        onChange={onChange}
+        onBlur={onBlur}
+        name={name}
       />
     </div>
   );
