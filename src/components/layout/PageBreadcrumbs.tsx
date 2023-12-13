fdd7f4c2b9c510e1d70cf7c9a9495580c73cb19a
Semi Invader <=>
2023-12-13 13:31:18 -0600

    tweak spacing and chevron sizes

--- src/components/layout/PageBreadcrumbs.tsx
+++ src/components/layout/PageBreadcrumbs.tsx
@@ -14,7 +14,7 @@ export default function PageBreadcrumbs() {
   const pathItems = ["Home"].concat(path.split("/"));
 
   return (
-    <div className="flex flex-row items-center gap-6">
+    <div className="flex flex-row items-center gap-2 sm:gap-4 md:gap-6">
       {pathItems.map((item, i) => {
         if (!item) return null;
         const isLast = pathItems.length - 1 == i;
@@ -26,25 +26,25 @@ export default function PageBreadcrumbs() {
               {isLast || notLink ? (
                 <span
                   className={classNames(
-                    "capitalize text-primary-text font-bold",
+                    "capitalize text-sm md:text-base text-primary-text font-bold",
                     notLink
                       ? "text-slate-500 dark:text-white/70 pointer-events-none"
                       : "text-primary-text"
                   )}
                 >
-                  {item}
+                  {item.replace(/-/g, " ")}
                 </span>
               ) : (
                 <Link
                   href={i == 0 ? "/" : "/" + item}
-                  className="capitalize font-bold hover:underline underline-offset-4 hover:text-primary-text/60"
+                  className="capitalize text-sm md:text-base font-bold hover:underline underline-offset-4 hover:text-primary-text/60"
                 >
-                  {item}
+                  {item.replace(/-/g, " ")}
                 </Link>
               )}
             </div>
             {isLast ? null : (
-              <div className="text-slate-500 dark:text-white/70 text-md">
+              <div className="text-slate-500 dark:text-white/70 text-xs md:text-md">
                 <FaChevronRight />
               </div>
             )}
