3448064849dba73a6d3be0516566ca63667331f5
Semi Invader <=>
2023-12-18 12:43:57 -0600

    line title and breadcrumbs

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -5,15 +5,15 @@ import PageBreadcrumbs from "@/components/layout/PageBreadcrumbs";
 export default function ExplorePage() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <section className="container max-w-7xl px-2 sm:px-12">
+      <section className="container max-w-7xl px-2 sm:px-8">
         <PageBreadcrumbs />
       </section>
-      <section className="container max-w-7xl px-12">
+      <section className="container max-w-7xl px-8">
         <h1 className="text-4xl font-semibold py-10 whitespace-pre-wrap">
           Explore NFTs
         </h1>
       </section>
-      <section className="">
+      <section className="max-w-7xl">
         <CardWrapper>
           <PreviewCard id={1} type="electric-guitar" />
           <PreviewCard id={1} type="electric-guitar" />
