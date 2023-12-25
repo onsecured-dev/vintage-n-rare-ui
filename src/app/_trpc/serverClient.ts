f8d25ddf4eef9b8b233f4e36567fb1255fb338af
Semi Invader <=>
2023-12-25 08:06:24 -0600

    added host as batch path

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -2,5 +2,5 @@ import { httpBatchLink } from "@trpc/client";
 import { appRouter } from "@/server";
 
 export const serverClient = appRouter.createCaller({
-  links: [httpBatchLink({ url: "https://localhost:3000/api/trpc" })],
+  links: [httpBatchLink({ url: process.env.HOST + "/api/trpc" })],
 })
